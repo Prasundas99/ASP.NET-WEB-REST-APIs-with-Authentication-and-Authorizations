14345460170b1be1d7485f0e823776d18b03e8be
Prasundas99 <prasundas115@gmail.com>
2022-05-04 19:06:57 +0530

    feat: GET and POST heros

---
+++ SuperHeroApi/Controllers/SuperHeroController.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuperHerosController : ControllerBase
+    {
+        private static List<SuperHeros> heros = new List<SuperHeros>{
+                new SuperHeros{
+                    Id = 1,
+                    Name = "Superman",
+                    FirstName = "Clark",
+                    LastName = "Kent",
+                    Place = "Metropolis"
+
+                },
+                new SuperHeros{
+                    Id = 2,
+                    Name = "Batman",
+                    FirstName = "Bruce",
+                    LastName = "Wayne",
+                    Place = "Gotham"
+                },
+                new SuperHeros{
+                    Id = 3,
+                    Name = "Spiderman",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York"
+                },
+                new SuperHeros{
+                    Id = 4,
+                    Name = "Tony",
+                    FirstName = "Stark",
+                    LastName = "Ironman",
+                    Place = "New York"
+                },
+            };
+
+
+        /**
+        * GET api/superheroes
+        * @Description: Get all superheroes
+        */
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHeros>>> Get()
+        {
+            return Ok(heros);
+        }
+
+        /**
+        * GET api/superheroes/{id}
+        * @Description: Get a superhero by id
+        */
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHeros>> Get(int id)
+        {
+            var hero = heros.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("Superhero not found");
+            return Ok(hero);
+        }
+
+
+        /**
+        * POST api/superheroes
+        * @Description: Create a new superhero
+        */
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHeros>>> AddHero(SuperHeros hero)
+        {
+            heros.Add(hero);
+            return Ok(heros);
+        }
+
+    }
+}
---
+++ SuperHeroApi/SuperHero.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperHeroApi
+{
+    public class SuperHeros
+    {
+        public int Id { get; set; }
+        public String Name { get; set; } = string.Empty;
+
+        public String FirstName { get; set; } = string.Empty;
+        public String LastName { get; set; } = string.Empty;
+        public string Place { get; set;} = string.Empty;
+    }
+}
